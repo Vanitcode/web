a64a957dff409c9bf6a23229d443ae6c12c88678
vanitcode <vanitcode@gmail.com>
2020-12-28 20:16:42 +0100

    Config y estructura principal cliente

--- src/ChatLan/Client.java
+++ src/ChatLan/Client.java
@@ -1,10 +1,129 @@
 package ChatLan;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
 public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		clientPanel client_panel = new clientPanel();
+		
+		Config prueba = new Config("VanitCode   -   Cliente", client_panel);
 	}
 
+	private JTextField nick, ip;
+	private JTextArea areaChat;
+}
+
+class clientPanel extends JPanelConfig implements Runnable{
+	
+	public clientPanel() {
+		super();
+				//Mejor evitar que se pueda maximizar
+				JPanel central_panel = new JPanel();
+				JPanel north_panel = new JPanel();
+				nick = new JTextField(5);
+				ip = new JTextField(8);
+				JLabel et1 = new JLabel("Chat");
+				JTextArea texto_cliente = new JTextArea(4,24);
+				JScrollPane scroll_text_cl = new JScrollPane(texto_cliente);
+				JButton enviar = new JButton("Send");
+				//enviar.setPreferredSize(new Dimension(15,2));
+				areaChat = new JTextArea(10,30);
+				enviar.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						
+						try {
+							Socket miSocket = new Socket("192.168.0.119",9999);
+							
+							EnvioPaqueteDatos datos = new EnvioPaqueteDatos();
+							datos.setNick(nick.getText());
+							datos.setIp(ip.getText());
+							datos.setTextoCliente(texto_cliente.getText());
+							
+							//Lo siguiente se puede hacer porque es Serializable
+							ObjectOutputStream flujoSalidaPaquete = new ObjectOutputStream(miSocket.getOutputStream());
+							flujoSalidaPaquete.writeObject(datos);
+							
+							miSocket.close();
+							
+							//Creación del flujo de datos
+							/*
+							DataOutputStream flujoSalida = new DataOutputStream(miSocket.getOutputStream());
+							flujoSalida.writeUTF(texto_cliente.getText());
+							flujoSalida.close();*/
+						}catch (UnknownHostException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+					
+				});
+				
+				north_panel.add(nick);
+				north_panel.add(et1);
+				north_panel.add(ip);
+				central_panel.add(areaChat);
+				central_panel.add(scroll_text_cl);
+				central_panel.add(enviar);
+				
+				add(north_panel, BorderLayout.NORTH);
+				add(central_panel, BorderLayout.CENTER);
+				
+				Thread miHilo = new Thread(this);
+				miHilo.start(); //llama al método run y ya siempre se hace lo de run
+	}
+	
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			ServerSocket escuchaCliente = new ServerSocket(9090);
+			Socket cliente;
+			EnvioPaqueteDatos paqueteRecibido;
+			
+			while(true) {
+				cliente = escuchaCliente.accept();
+				ObjectInputStream flujoEntrada = new ObjectInputStream (cliente.getInputStream());
+				paqueteRecibido = (EnvioPaqueteDatos) flujoEntrada.readObject();
+				
+				areaChat.append("\n" + paqueteRecibido.getNick() + ": " + paqueteRecibido.getTextoCliente());
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private JTextField nick, ip;
+	private JTextArea areaChat;
 }
--- src/ChatLan/Config.java
+++ src/ChatLan/Config.java
@@ -1,5 +1,76 @@
 package ChatLan;
 
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.Serializable;
+
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
 public class Config {
 
+	public Config(String title, JPanel panel) {
+		
+		FrameConfig frame = new FrameConfig();
+		frame.setTitle(title);
+		frame.setBounds(300, 200, 800, 450);
+		frame.setDefaultCloseOperation(3);
+		frame.add(panel);
+		frame.setVisible(true);
+	}
+}
+
+class FrameConfig extends JFrame{
+	
+	public FrameConfig() {
+		Toolkit miToolkit = Toolkit.getDefaultToolkit();
+		Image logo = miToolkit.getImage("icons\\vanitcode.png");
+		setIconImage(logo);
+	}
+}
+
+class JPanelConfig extends JPanel{
+	
+	public JPanelConfig() {
+		setLayout(new BorderLayout());
+		JLabel imagenFooter = new JLabel();
+		imagenFooter.setIcon(new ImageIcon("icons\\vanitcode.png"));
+		add(imagenFooter, BorderLayout.SOUTH);
+	}
+}
+
+class EnvioPaqueteDatos implements Serializable{ //Para que sea susceptible de serializarse y poder enviar el paquete
+	
+	private String nick, ip, textoCliente;
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getTextoCliente() {
+		return textoCliente;
+	}
+
+	public void setTextoCliente(String textoCliente) {
+		this.textoCliente = textoCliente;
+	}
+	
+	
 }
